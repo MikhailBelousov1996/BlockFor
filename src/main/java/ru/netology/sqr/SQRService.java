a75773d6bfd1a4f6f7feb296304a59c088c404fe
Mihal Belousov <wroomwroom1996@gmail.com>
2021-11-09 21:22:55 +0300

    modified code

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,7 +4,7 @@ import javax.swing.plaf.IconUIResource;
 
 public class SQRService {
     public int calculateSQRT(int minNumber, int maxNumber) {
-        int counter=0;
+        int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= minNumber) {
                 if (i * i <= maxNumber) {
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -11,7 +11,43 @@ class SQRServiceTest {
         SQRService sqrService = new SQRService();
 
         int expected = 3;
-        int actual = sqrService.calculateSQRT(200,300);
+        int actual = sqrService.calculateSQRT(200, 300);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberSQRTMinDiapazone() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 2;
+        int actual = sqrService.calculateSQRT(150, 200);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberSQRTVeryBigDiapazone() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 90;
+        int actual = sqrService.calculateSQRT(10, 2000000000);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberSQRTNonSQRT() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 0;
+        int actual = sqrService.calculateSQRT(10, 20);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberSQRTOneSQRT() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 1;
+        int actual = sqrService.calculateSQRT(10, 100);
         assertEquals(expected, actual);
     }
 }
